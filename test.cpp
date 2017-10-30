3b716ae05b227e368946c123f479036faf3176bc
Nikolai Prigodich <1lampard@mail.ru>
2017-10-30 21:43:36 +0300

    remove circular buffer. half extract particle effect class

--- test.cpp
+++ test.cpp
@@ -25,30 +25,88 @@ static volatile bool workerMustExit = false;
 
 
 // some code
-class CParticle
+class Particle
 {
 
 public:
-//	Vector2	m_old_pos;
+	//	Vector2	m_old_pos;
 	Vector2	m_pos;
 	Vector2	m_vel;
 	void	Update(float time);
 };
 
-void CParticle::Update(float time)
+void Particle::Update(float time)
 {
-//	m_old_pos = m_pos;
+	//	m_old_pos = m_pos;
 	Vector2 pos2 = m_pos + m_vel * time;
 
 	// bit of randomness
-//	m_vel += Vector2(rand() % 10000 / 1000.0f - 5.0f, rand() % 10000 / 1000.0f - 5.0f);
+	//	m_vel += Vector2(rand() % 10000 / 1000.0f - 5.0f, rand() % 10000 / 1000.0f - 5.0f);
 	// bit of gravity
 	m_vel -= Vector2(0.0f, 1.0f);
 	// friction
-//	m_vel -= m_vel * 0.002f;
+	//	m_vel -= m_vel * 0.002f;
 
 	m_pos = pos2;
 }
+
+
+
+
+
+
+
+
+class ParticleEffect
+{
+
+public:
+	Particle* particles;
+	int count;
+	float birthTime;
+
+	~ParticleEffect();
+	void	Update(float time);
+	void Init(int n);
+	void updateOffscreen(int i);
+	bool removeOld(int& i);
+	void Render();
+	void Emit(int x, int y);
+};
+
+ParticleEffect::~ParticleEffect()
+{
+	if (particles)
+		delete[] particles;
+}
+
+void ParticleEffect::Init(int n)
+{
+	particles = new Particle[n];
+	count = n;
+	birthTime = -1;
+}
+
+void ParticleEffect::Update(float time)
+{
+	for (int i = 0; i < count; ++i)
+	{
+		particles[i].Update(time);
+	}
+}
+
+void ParticleEffect::Render()
+{
+	for (int i = 0; i < count; i++)
+	{
+		if (birthTime > 0)
+			platform::drawPoint(particles[i].m_pos.x, particles[i].m_pos.y, 1, 1, 1, 1);
+	}
+}
+
+
+
+
 
 
 
@@ -66,14 +124,16 @@ public:
 	void Emit(int x, int y);
 //	private:
 	int			m_num_particles;
-	circular_buffer<CParticle> mp_particles;
+	ParticleEffect* mp_particles;
 //	circular_buffer<bool> isAlive;
-	circular_buffer<float> birthTime;
+//	float* birthTime;
 
-//	int lastParticleId;
-//	int nextDeadParticleId;
+	int lastParticleId;
+	int nextDeadParticleId;
 
-//	int activeParticlesCount;
+	int activeParticlesCount;
+
+	std::mutex mutex_;
 };
 
 CParticleManager	g_ParticleManager;
@@ -82,32 +142,33 @@ std::list<Vector2> activeParticlesPositions;
 
 CParticleManager::CParticleManager()
 {
-	//	mp_particles (m_num_particles);
+		mp_particles = nullptr;
+//		birthTime = nullptr;
 	//	isAlive = NULL;
 	m_num_particles = 0;
 
-//	lastParticleId = 0;
-//	nextDeadParticleId = 0;
-//	activeParticlesCount = 0;
+	lastParticleId = 0;
+	nextDeadParticleId = 0;
+	activeParticlesCount = 0;
 }
 
 CParticleManager::~CParticleManager()
 {
-//	if (mp_particles)
-///		delete[] mp_particles;
+	if (mp_particles)
+		delete[] mp_particles;
 ///
 ///	if (isAlive)
 ///		delete[] isAlive;
 ///
-///	if (birthTime)
-///		delete[] birthTime;
+	if (birthTime)
+		delete[] birthTime;
 }
 
 void CParticleManager::Init(int n)
 {
-	mp_particles.reserve(n);
+	mp_particles = new ParticleEffect[n];
 //	isAlive.reserve(n);
-	birthTime.reserve(n);
+	birthTime = new float[n];
 	m_num_particles = n;
 	for (int i = 0; i<n; i++)
 	{
@@ -129,25 +190,7 @@ void CParticleManager::updateOffscreen(int i)
 //	nvtxRangePop();
 }
 
-bool CParticleManager::removeOld(int& i)
-{
-//	nvtxRangePush(__FUNCTION__);
 
-	float time = globalTime.load();
-	bool res = false;
-	if (birthTime[i] > 0 && birthTime[i] + 2000 < time)
-	{
-		mp_particles.removeLast();
-		birthTime.removeLast();
-		if (rand() % 10 == 0)
-			Emit(mp_particles[i].m_pos.x, mp_particles[i].m_pos.y);
-		i--;
-		res = true;
-	}
-
-//	nvtxRangePop();
-	return res;
-}
 
 // Call the particle update function, either directly
 // or by starting the thread(s) and then waiting for them to finish
@@ -162,12 +205,27 @@ void CParticleManager::Update(float time)
 	nvtxRangePush(__FUNCTION__);
 
 	
-
-	for (int i = 0; i < mp_particles.size(); i++)
+	int count = activeParticlesCount;
+	int last = lastParticleId;
+	for (int index = 0; index < count; index++)
 	{
+		int i = (last + index) % m_num_particles;
 		updateOffscreen(i);
 
-		if (removeOld(i)) continue;
+		//	nvtxRangePush(__FUNCTION__);
+
+		float t = globalTime.load();
+		if (birthTime[i] > 0 && birthTime[i] + 2000 < t)
+		{
+			birthTime[i] = -1;
+			lastParticleId++;
+			activeParticlesCount--;
+					if (rand() % 10 == 0)
+						Emit(mp_particles[i].m_pos.x, mp_particles[i].m_pos.y);
+			
+		}
+
+		//	nvtxRangePop();
 
 		if (birthTime[i] > 0)
 		{
@@ -180,8 +238,9 @@ void CParticleManager::Update(float time)
 
 void CParticleManager::Render()
 {
-	for (int i = 0; i < mp_particles.size(); i++)
+	for (int index = 0; index < activeParticlesCount; index++)
 	{
+		int i = (lastParticleId + index) % m_num_particles;
 		if (birthTime[i] > 0)
 			platform::drawPoint(mp_particles[i].m_pos.x, mp_particles[i].m_pos.y, 1, 1, 1, 1);
 	}
@@ -189,27 +248,37 @@ void CParticleManager::Render()
 
 void CParticleManager::Emit(int x, int y)
 {
-//	int start = nextDeadParticleId;
-//	int end = std::min(start + 64, m_num_particles);
-//	nextDeadParticleId = end % m_num_particles;
-//	activeParticlesCount += 64;
-	//fix buffer overlaping
+	int start = nextDeadParticleId;
+	int end = std::min(start + 64, m_num_particles);
+	nextDeadParticleId = end % m_num_particles; //error fix last effect
+	
 
+	if (lastParticleId == start && activeParticlesCount > 0)
+	{
+		lastParticleId += 64;
+	}
+	else
+	{
+		activeParticlesCount += 64;
+	}
+
+	//fix buffer overlaping
+//	std::lock_guard<std::mutex> lock(mutex_);
 	nvtxRangePush(__FUNCTION__);
 
-	for (int i = 0; i<64; i++)
+	for (int i = start; i<end; i++)
 	{
 //		isAlive.put(true);
-		float& time = birthTime.put();
-		time = globalTime.load(); //store in variable
+		birthTime[i] = globalTime.load(); //store in variable
 
-		CParticle& particle = mp_particles.put();
-		particle.m_pos = Vector2(x, y);
-		particle.m_vel = Vector2(rand() % 100 - 50, rand() % 100 - 50);
-		particle.m_vel = particle.m_vel.Normal();
-		particle.m_vel = particle.m_vel * (rand() % 100);
+		mp_particles[i].m_pos = Vector2(x, y);
+//		mp_particles[i].m_vel = Vector2(0, 0);
+		mp_particles[i].m_vel = Vector2(rand() % 100 - 50, rand() % 100 - 50);
+		mp_particles[i].m_vel = mp_particles[i].m_vel.Normal();
+		mp_particles[i].m_vel = mp_particles[i].m_vel * (rand() % 100);
 
-		
+//		activeParticlesCount++;
+//		nextDeadParticleId++;
 	}
 	
 	nvtxRangePop();
@@ -231,9 +300,9 @@ void WorkerThread(void)
 //		std::cout << delta << "\n";
 		g_ParticleManager.Update(delta / 1000.f);
 		
-		if (delta < 100)
+		if (delta < 10)
 		{
-			int duration = 100 - static_cast<int>(delta/**1000.f*/);
+			int duration = 10 - static_cast<int>(delta/**1000.f*/);
 //			if (duration != 10)
 //			{
 //				printf("%i", duration);
@@ -285,7 +354,7 @@ void test::update(int dt)
 {
 	// some code
 	
-	std::cout << g_ParticleManager.mp_particles.size() << "\n";
+	std::cout << g_ParticleManager.activeParticlesCount << "\n";
 //	g_ParticleManager.Update(dt / 1000.f);
 
 	float time = globalTime.load();

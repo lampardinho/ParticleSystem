efc9b5daac44f872bcdb4239d60c13cd4fa7c406
Nikolai Prigodich <1lampard@mail.ru>
2017-10-31 21:07:33 +0300

    tripple buffering wip

--- test.cpp
+++ test.cpp
@@ -14,6 +14,7 @@
 #include <list>
 #include "CircularBuffer.h"
 #include <condition_variable>
+#include <future>
 
 
 // Just some hints on implementation
@@ -44,7 +45,7 @@ void InitializeSettings()
 	g_CurrentSettings.particleMaxInitialVelocity = 100;
 	g_CurrentSettings.particleLifetime = 2000;
 	g_CurrentSettings.gravity = 1;
-	g_CurrentSettings.emitOnDestroyProbability = 0.5;
+	g_CurrentSettings.emitOnDestroyProbability = 0.05;
 }
 
 
@@ -56,22 +57,35 @@ public:
 	Vector2	m_pos;
 	Vector2	m_vel;
 	bool isVisible = true;
+	bool isNew = false;
 	void	Update(const Particle& lastState, float time);
 };
 
 void Particle::Update(const Particle& lastState, float time)
 {
-	if (lastState.m_pos.x < 0 || lastState.m_pos.x > test::SCREEN_WIDTH)
+	Vector2 pos = lastState.m_pos;	
+	Vector2 vel = lastState.m_vel;
+	bool vis = lastState.isVisible;
+	
+	if (isNew)
+	{
+		isNew = false;
+		pos = m_pos;
+		vel = m_vel;
+		vis = isVisible;
+	}
+
+	if (pos.x < 0 || pos.x > test::SCREEN_WIDTH)
 		isVisible = false;
 
-	if (lastState.m_pos.y < 0 || lastState.m_pos.y > test::SCREEN_HEIGHT)
+	if (pos.y < 0 || pos.y > test::SCREEN_HEIGHT)
 		isVisible = false;
 
-	if (!lastState.isVisible)
+	if (!vis)
 		return;
 
-	Vector2 pos2 = lastState.m_pos + lastState.m_vel * time;
-	m_vel = lastState.m_vel - Vector2(0.0f, g_CurrentSettings.gravity);
+	Vector2 pos2 = pos + vel * time;
+	m_vel = vel - Vector2(0.0f, g_CurrentSettings.gravity);
 	m_pos = pos2;
 }
 
@@ -124,7 +138,7 @@ public:
 	void Init(int n);
 	void updateOffscreen(int i);
 	bool removeOld(int& i);
-	void Update(float time);
+	void Update(int first, int end, float time);
 	void Render();
 	void Emit(int x, int y);
 	void SwapRenderBuffer();
@@ -134,8 +148,9 @@ public:
 
 	ParticleBuffer* buffers[3];
 	int renderBufferId = 0;
-	int lastUpdatedBufferId = 0;
-	int currentUpdatingBufferId = 1;
+	int lastUpdatedBufferId = 1;
+	int currentUpdatingBufferId = 2;
+	int nextToUpdateBufferId = 1;
 
 //	int lastParticleId;
 //	int nextDeadParticleId;
@@ -187,7 +202,7 @@ void CParticleManager::Init(int n)
 }
 
 
-void CParticleManager::Update(float time)
+void CParticleManager::Update(int first, int end, float time)
 {
 //	std::lock_guard<std::mutex> lock(mutex_);
 
@@ -196,7 +211,8 @@ void CParticleManager::Update(float time)
 	ParticleBuffer* buffer = buffers[currentUpdatingBufferId];
 	int count = buffer->activeParticlesCount;
 	int last = buffer->lastParticleId;
-	for (int index = 0; index < count; index++)
+
+	for (int index = first; index < end; index++)
 	{
 		int i = (last + index) % m_num_particles;
 
@@ -231,7 +247,7 @@ void CParticleManager::Render()
 
 void CParticleManager::Emit(int x, int y)
 {
-	std::cout << currentUpdatingBufferId << "\n";
+	std::cout << nextToUpdateBufferId << "\n";
 //
 //	if (activeParticlesCount > m_num_particles)
 //		std::cout << "alert\n";
@@ -250,28 +266,55 @@ void CParticleManager::Emit(int x, int y)
 
 
 
-	buffer = buffers[lastUpdatedBufferId];
-	buffer->effects[buffer->nextDeadParticleId].Emit(x, y);
 
-	buffer->nextDeadParticleId = (buffer->nextDeadParticleId + 1) % m_num_particles;
-
-	if (buffer->nextDeadParticleId == buffer->lastParticleId)
-		buffer->lastParticleId = (buffer->lastParticleId + 1) % m_num_particles;
-	else
-		buffer->activeParticlesCount++;
+//	buffer = buffers[currentUpdatingBufferId];
+//	buffer->effects[buffer->nextDeadParticleId].Emit(x, y);
+//
+//	buffer->nextDeadParticleId = (buffer->nextDeadParticleId + 1) % m_num_particles;
+//
+//	if (buffer->nextDeadParticleId == buffer->lastParticleId)
+//		buffer->lastParticleId = (buffer->lastParticleId + 1) % m_num_particles;
+//	else
+//		buffer->activeParticlesCount++;
+//
+//
+//
+//
+//
+//	buffer = buffers[renderBufferId];
+//	buffer->effects[buffer->nextDeadParticleId].Emit(x, y);
+//	buffer->nextDeadParticleId = (buffer->nextDeadParticleId + 1) % m_num_particles;
+//
+//	if (buffer->nextDeadParticleId == buffer->lastParticleId)
+//		buffer->lastParticleId = (buffer->lastParticleId + 1) % m_num_particles;
+//	else
+//		buffer->activeParticlesCount++;
 }
 
 void CParticleManager::SwapRenderBuffer()
 {
-//	std::lock_guard<std::mutex> lock(mutex_);
-//
-//	std::swap(renderBufferId, lastUpdatedBufferId);
+	std::lock_guard<std::mutex> lock(mutex_);
+
+	if (renderBufferId == lastUpdatedBufferId)
+		return;
+
+	nextToUpdateBufferId = renderBufferId;
+	renderBufferId = lastUpdatedBufferId;
+
+	std::cout << "render: " << renderBufferId << "\tlast updated: " << lastUpdatedBufferId << "\tcurrent: " << currentUpdatingBufferId << "\tnext: " << nextToUpdateBufferId << " render\n";
 }
 
 void CParticleManager::SwapUpdateBuffer()
 {
-	std::swap(currentUpdatingBufferId, lastUpdatedBufferId);
-	std::cout << currentUpdatingBufferId << " " << renderBufferId << " " << lastUpdatedBufferId << "\n";
+	std::lock_guard<std::mutex> lock(mutex_);
+
+	lastUpdatedBufferId = currentUpdatingBufferId;
+	currentUpdatingBufferId = nextToUpdateBufferId;
+	nextToUpdateBufferId = lastUpdatedBufferId;
+	
+	std::cout << "render: " << renderBufferId << "\tlast updated: " << lastUpdatedBufferId << "\tcurrent: " << currentUpdatingBufferId << "\tnext: " << nextToUpdateBufferId << " update\n";
+
+	buffers[currentUpdatingBufferId]->nextDeadParticleId = buffers[lastUpdatedBufferId]->nextDeadParticleId;
 }
 
 
@@ -323,6 +366,7 @@ void ParticleEffect::Emit(int x, int y)
 
 	for (int i = 0; i < count; i++)
 	{
+		particles[i].isNew = true;
 		particles[i].isVisible = true;
 		particles[i].m_pos = Vector2(x, y);
 		particles[i].m_vel = Vector2(rand() % 101 - 50, rand() % 101 - 50);
@@ -365,14 +409,35 @@ void WorkerThread(void)
 
 		// some code
 //		std::cout << delta << "\n";
-		g_ParticleManager.Update(delta / 1000.f);
 
-		std::unique_lock<std::mutex> lock(g_ParticleManager.mutex_);
-		while (!g_ParticleManager.rendered) {  // loop to avoid spurious wakeups
-			g_ParticleManager.cond_var.wait(lock);
+		ParticleBuffer* buffer = g_ParticleManager.buffers[g_ParticleManager.currentUpdatingBufferId];
+		int count = buffer->activeParticlesCount;
+
+		int first = 0;
+		float dt = delta / 1000.f;
+
+		unsigned long const max_chunk_size = 25;
+		if (count <= max_chunk_size)
+		{
+			g_ParticleManager.Update(first, count, dt);
 		}
+		else
+		{
+			int mid_point = count / 2;
+			std::future<void> first_half = std::async(&CParticleManager::Update, &g_ParticleManager, first, mid_point, dt);
+			g_ParticleManager.Update(mid_point, count, dt);
+			first_half.get();
+		}
+
+
+//		g_ParticleManager.Update(delta / 1000.f);
+
+//		std::unique_lock<std::mutex> lock(g_ParticleManager.mutex_);
+//		while (!g_ParticleManager.rendered) {  // loop to avoid spurious wakeups
+//			g_ParticleManager.cond_var.wait(lock);
+//		}
 		g_ParticleManager.SwapUpdateBuffer();
-		g_ParticleManager.rendered = false;
+//		g_ParticleManager.rendered = false;
 
 		if (delta < 10)
 		{
@@ -418,11 +483,11 @@ void test::render(void)
 	
 
 	// some code
-	std::unique_lock<std::mutex> lock(g_ParticleManager.mutex_);
+//	std::unique_lock<std::mutex> lock(g_ParticleManager.mutex_);
 	g_ParticleManager.Render();
-//	g_ParticleManager.SwapRenderBuffer();
-	g_ParticleManager.rendered = true;
-	g_ParticleManager.cond_var.notify_one();
+	g_ParticleManager.SwapRenderBuffer();
+//	g_ParticleManager.rendered = true;
+//	g_ParticleManager.cond_var.notify_one();
 
 	// some code
 }
